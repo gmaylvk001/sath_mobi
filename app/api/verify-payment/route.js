1844c6167297d1b13c279bfbcb3b3d12841cd0d9
Abitha <ramesh@eywamedia.com>
2026-06-01 15:37:14 +0530

    sm make payment gateway live

--- app/api/verify-payment/route.js
+++ app/api/verify-payment/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 import crypto from 'crypto'; // Add crypto import
 
 const razorpay = new Razorpay({
-  key_id: process.env.NEXT_PUBLIC_RAZORPAY_TEST_KEY || process.env.NEXT_PUBLIC_RAZORPAY_LIVE_KEY,
+  key_id: process.env.NEXT_PUBLIC_RAZORPAY_LIVE_KEY || process.env.NEXT_PUBLIC_RAZORPAY_TEST_KEY,
   key_secret: process.env.RAZORPAY_SECRET
 });
 
