f3042e4a74d3ba1c10e65b14eb39783e412f8a73
Abitha <ramesh@eywamedia.com>
2026-06-16 14:51:09 +0530

    SM change about banner

--- components/index.js
+++ components/index.js
@@ -395,7 +395,7 @@ function CategoryCard({ image, title, bg }) {
       <div>
         <Link href="/aboutus">
           <Image
-            src="/assets/images/sathya-banner4.png"
+            src="/assets/images/sm-about-banner.png"
             alt="Sony Banner"
             width={1920}
             height={600}
