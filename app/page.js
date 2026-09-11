3c96e6479bde6d07c7413805dc1f0aef51d40c1a
muneeswaran <muneeswaran@eywamedia.com>
2026-09-11 18:52:04 +0530

    SM h1 tag home page

--- app/page.js
+++ app/page.js
@@ -4,8 +4,9 @@ import IndexComponent from "../components/index";
 export default function Home() {
   return (
     <div className="bg-linear-to-r from-linearyellow via-white to-linearyellow">
-    <h1 className="text-primary font-bold text-2xl mb-3">What&apos;s Hot</h1>
+    
     <IndexComponent />
+    <h1 className="text-primary font-bold text-2xl mb-3">&nspb;</h1>
     </div>
   );
 }
